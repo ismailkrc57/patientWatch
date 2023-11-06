e47c56d0cc3cf8668fbc0f9842a1dd7e9c24b3ae
ismail.karaca <ismailkrc57@gmail.com>
2023-11-06 22:45:28 +0300

    add patient

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WatchComponent} from "./watch/watch.component";
+import {DoctorComponent} from "./doctor/doctor.component";
+import {PatientComponent} from "./patient/patient.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'watch', component: WatchComponent},
+  {path: 'doctor', component: DoctorComponent},
+  {path: 'patient', component: PatientComponent},
+  {path: '', redirectTo: '/watch', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
